d96aa15218fa5025f68d826fe951f56bb74546d2
Germán Arellano Pozo <germanarellano26@gmail.com>
2021-01-13 01:35:07 -0500

    new changes.

--- src/main/java/com/serendipiaTechnology/springbootsoapexample/endpoint/UserEndpoint.java
+++ src/main/java/com/serendipiaTechnology/springbootsoapexample/endpoint/UserEndpoint.java
@@ -1,10 +1,9 @@
 package com.serendipiaTechnology.springbootsoapexample.endpoint;
 
-import com.serendipiaTechnology.spring_boot_soap_example.GetUserRequest;
-import com.serendipiaTechnology.spring_boot_soap_example.GetUserResponse;
 import com.serendipiaTechnology.springbootsoapexample.service.UserService;
+import com.serendipiatechnology.spring_boot_soap_example.GetUserRequest;
+import com.serendipiatechnology.spring_boot_soap_example.GetUserResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
@@ -16,16 +15,12 @@ public class UserEndpoint {
     @Autowired
     private UserService userService;
 
-    private RestTemplate restTemplate;
-
-
     @PayloadRoot(namespace = "http://serendipiaTechnology.com/spring-boot-soap-example",
             localPart = "getUserRequest")
     @ResponsePayload
     public GetUserResponse getUserRequest(@RequestPayload GetUserRequest request) {
         GetUserResponse response = new GetUserResponse();
         response.setUser(userService.getUsers(request.getName()));
-        GetUserResponse getUserResponse = restTemplate.getForEntity("http://lolcalhost:8080/User/authenticate", GetUserResponse.class, request).getBody();
         return response;
     }
 }
--- src/main/java/com/serendipiaTechnology/springbootsoapexample/service/UserService.java
+++ src/main/java/com/serendipiaTechnology/springbootsoapexample/service/UserService.java
@@ -1,6 +1,6 @@
 package com.serendipiaTechnology.springbootsoapexample.service;
 
-import com.serendipiaTechnology.spring_boot_soap_example.User;
+import com.serendipiatechnology.spring_boot_soap_example.User;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -15,30 +15,13 @@ public class UserService {
     @PostConstruct
     public void initialize() {
 
-        User peter = new User();
-        peter.setName("Peter");
-        peter.setEmpId(1111);
-        peter.setSalary(12000);
-
-        User sam = new User();
-        sam.setName("Sam");
-        sam.setEmpId(1112);
-        sam.setSalary(32000);
-        User ryan = new User();
-        ryan.setName("Ryan");
-        ryan.setEmpId(1113);
-        ryan.setSalary(16000);
         User renny = new User();
-        ryan.setName("Renny Donny");
-        ryan.setEmpId(2021);
-        ryan.setSalary(99999);
+        renny.setName("Renny Donny");
+        renny.setEmpId(111);
+        renny.setSalary(999999);
 
-        users.put(peter.getName(), peter);
-        users.put(sam.getName(), sam);
-        users.put(ryan.getName(), ryan);
         users.put(renny.getName(),renny);
     }
-
 
     public User getUsers(String name) {
         return users.get(name);
